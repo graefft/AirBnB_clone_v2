fe86aaab11ef7b34e8a04ec8d00d193cc1c14c1d
Thomas Graeff <676@holbertonschool.com>
2019-08-20 06:50:01 +0000

    Makes great .tgz file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,11 +10,11 @@ def do_pack():
     local('mkdir -p versions')
     date = datetime.now()
     name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(date.hour,
-                                                date.month,
-                                                date.day,
-                                                date.hour,
-                                                date.minute,
-                                                date.second)
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
     command = local('tar -cvzf ' + name + ' ./web_static')
     if command.succeeded:
         return name
