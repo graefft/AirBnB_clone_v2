ed3550b883568abcad854bee007d29459a0ab94c
Thomas Graeff <676@holbertonschool.com>
2019-08-24 05:32:20 +0000

    Adds deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Fabric'''
+'''Fabric script to deploy web_static on server'''
 
 
 import os
@@ -17,15 +17,26 @@ def do_deploy(archive_path):
 
     base = os.path.basename(archive_path)
     name = archive_path.split('.')[0]
-    directory = '/data/web_static/releases/'
-    run('mkdir -p {}{}'.format(directory, name))
-    run('tar -xzf /tmp/{} -C {}{}'.format(base,
-                                          directory,
-                                          name))
-    run('rm -rf /tmp/' + base)
-    run('rm -rf /data/web_static/current')
-    run('ln -sf {}{} /data/web_static/current'.format(directory,
-                                                      name))
-    run('service nginx restart')
+    no_ext = name.split('/')[1]
 
+    directory = '/data/web_static/releases/'
+    result = put(archive_path, "/tmp/".format(name))
+    if result.failed:
+        return False
+
+    run('mkdir -p {}{}/'.format(directory, no_ext))
+    run('tar -xzf /tmp/{} -C {}{}/'.format(base,
+                                           directory,
+                                           no_ext))
+    run('rm /tmp/' + base)
+    run('mv {}{}/web_static/* {}{}/'.format(directory,
+                                            no_ext,
+                                            directory,
+                                            no_ext))
+    run('rm -rf {}{}/web_static'.format(directory,
+                                        no_ext))
+    run('rm -rf /data/web_static/current')
+    run('ln -sf {}{}/ /data/web_static/current'.format(directory,
+                                                       name))
+    print('New version deployed!')
     return True
