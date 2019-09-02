b82e248d3525873dfadd5f551071c93feef9a463
Thomas Graeff <676@holbertonschool.com>
2019-09-02 01:41:58 +0000

    Adds script to print python/<text>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''starts Flask web app with /python/<text>'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    '''prints Hello HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''print HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''prints C followed by input text'''
+    c_text = 'C {}'.format(text)
+    c_text = c_text.replace('_', ' ')
+    return c_text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_route(text='is cool'):
+    '''prints Python followed by input text'''
+    py_text = 'Python {}'.format(text)
+    py_text = py_text.replace('_', ' ')
+    return py_text
+
+
+if __name__  == '__main__':
+    app.run(host='0.0.0.0', port=5000)
