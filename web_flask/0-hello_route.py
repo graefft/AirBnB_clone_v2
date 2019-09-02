67df2c2441dc3ad9e34c12012e58b3ccee57f5d9
Thomas Graeff <676@holbertonschool.com>
2019-09-02 01:30:54 +0000

    Adds documentation comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+'''Starts Flask web app'''
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    '''prints Hello HBNB'''
     return 'Hello HBNB!'
 app.run(host='0.0.0.0', port=5000)
