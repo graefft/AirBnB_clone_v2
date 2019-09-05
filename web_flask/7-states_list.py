2e248120fca4b3392257ba58b8725dea2b6553a3
Thomas Graeff <676@holbertonschool.com>
2019-09-05 03:01:33 +0000

    Adds states_list Flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,16 +7,19 @@ from models import storage, State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def clean_up(self):
     '''closes storage'''
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     '''lists all states in db'''
     state_dict = storage.all(State)
     return render_template('7-states_list.html', state_dict=state_dict)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
