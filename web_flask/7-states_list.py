54fb146d2bbbdf0c20fcc732e905fa30db5ad75a
Thomas Graeff <676@holbertonschool.com>
2019-09-05 04:02:11 +0000

    Add states_list with correct name

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,22 +3,23 @@
    fetching data from storage engine
 '''
 from flask import Flask, render_template
-from models import storage, State
+from models import storage
+from models import State
 
 app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    '''lists all states in db'''
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def clean_up(self):
     '''closes storage'''
     storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def state_list():
-    '''lists all states in db'''
-    state_dict = storage.all(State)
-    return render_template('7-states_list.html', state_dict=state_dict)
 
 
 if __name__ == '__main__':
